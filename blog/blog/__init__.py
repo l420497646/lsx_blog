a184bec1cd8244edbc643cf578905f67669ac2a1
l420497646 <liusixuan1996@163.com>
2023-10-31 18:04:36 +0800

    '配置静态资源'

--- blog/blog/__init__.py
+++ blog/blog/__init__.py
@@ -1,3 +1,14 @@
 import pymysql
 
+import logging
+ 
+
+
 pymysql.install_as_MySQLdb()
+
+# 创建日志记录器
+logger = logging.getLogger('django')
+# 输出日志
+logger.debug('测试logging模块debug')
+logger.info('测试logging模块info')
+logger.error('测试logging模块error')
